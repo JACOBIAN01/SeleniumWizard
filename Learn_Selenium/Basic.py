34cd96e271327c5beeafb5b459a8f4cee2a57541
JACOBIAN01 <subhadeep23@gmail.com>
2025-03-29 23:04:50 +0530

    Update

--- Learn_Selenium/Basic.py
+++ Learn_Selenium/Basic.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 import os , time
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
-driver.get("https://www.codingal.com/login/")
+# driver.get("https://www.codingal.com/login/")
 
 # element = driver.find_element(By.ID,"Welcome_to_Wikipedia")
 
@@ -12,17 +12,32 @@ driver.get("https://www.codingal.com/login/")
 
 # element = driver.find_element(By.XPATH,"//div[@id='mp-left']")
 
-element = driver.find_element(By.ID,"phone")
-time.sleep(1)
-element.send_keys("7029043892")
-time.sleep(1)
-login = driver.find_element(By.XPATH,"//button[contains(text(),'Login with password')]")
-time.sleep(1)
-login.click()
-time.sleep(2)
-password_input = driver.find_element(By.ID,"password")
-password_input.send_keys("Chiku@(2003)")
-login_again = driver.find_element(By.XPATH,"//button[contains(text(),'Login with password')]")
-login_again.click()
-# print(element.text)
-time.sleep(5)
+# element = driver.find_element(By.ID,"phone")
+# time.sleep(1)
+# element.send_keys("")
+# time.sleep(1)
+# login = driver.find_element(By.XPATH,"//button[contains(text(),'Login with password')]")
+# time.sleep(1)
+# login.click()
+# time.sleep(2)
+# password_input = driver.find_element(By.ID,"password")
+# password_input.send_keys("")
+# login_again = driver.find_element(By.XPATH,"//button[contains(text(),'Login with password')]")
+# login_again.click()
+# # print(element.text)
+# time.sleep(5)
+
+
+
+# driver.get("https://vtop.vitap.ac.in/vtop/open/page")
+
+# student_div= driver.find_element(By.XPATH,"//div[contains(.,'Student')]")
+
+# student_button = student_div.find_element(By.TAG_NAME,"button")
+
+# student_button.click()
+
+
+
+
+
