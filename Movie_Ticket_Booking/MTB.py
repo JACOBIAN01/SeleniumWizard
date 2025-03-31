9bdb832fd07cadedd7c106e41472b500d49c5360
JACOBIAN01 <subhadeep23@gmail.com>
2025-03-31 14:45:41 +0530

    Update

--- Movie_Ticket_Booking/MTB.py
+++ Movie_Ticket_Booking/MTB.py
@@ -3,6 +3,15 @@ import os , time
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(2)
-driver.get("https://in.bookmyshow.com/explore/home/kharagpur")
-driver.implicitly_wait(2)
+driver.implicitly_wait(5)
+driver.get("https://in.bookmyshow.com/explore/home/vijayawada")
+
+Movies = driver.find_elements(By.XPATH,"//div[contains(@class, 'sc-7o7nez-0') and contains(@class, 'daKrZU')]")
+driver.implicitly_wait(10)
+
+print(Movies)
+# for movie in Movies:
+#     print(movie.text)
+driver.implicitly_wait(10)
+
+driver.quit()
